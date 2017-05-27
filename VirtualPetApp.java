a5d745e151258c193cfb13fd56e1937d11fa1df9
Derek <daanderso@gmail.com>
2017-05-27 13:58:04 -0400

    latest updates

--- VirtualPetApp.java
+++ VirtualPetApp.java
@@ -26,16 +26,10 @@ public class VirtualPetApp {
 		writeLine("Hunger: " + dragon.getHunger());
 		writeLine("Thirst: " + dragon.getThirst());
 		writeLine("Boredom:"  + dragon.getPlayfulness());
+		writeLine("Tiredness:" + dragon.getTiredness());
 		
 		//while loop
 		while (!dragon.death){
-	
-		//Virtual Pet Menu
-		//writeLine("Drago the Dragon");		
-		//writeLine("Before Tick:");
-		//writeLine("Hunger: " + dragon.getHunger());
-		//writeLine("Thirst: " + dragon.getThirst());
-		//writeLine("Boredom:"  + dragon.getPlayfulness());
 		
 		writeLine("What do you want to do ?\n");
 		writeLine("1. Feed Drago");
@@ -48,37 +42,45 @@ public class VirtualPetApp {
 		
 		switch (enterAction) {
 		case "1":
+			//Feed Dragon
 			dragon.feed();
-			//writeInt(dragon.getHunger());
-			//drago is hungry
+			//Drago is hungry
 			if (dragon.isHungry()){
-			writeLine("Feed ME !!!");
+			writeLine("Feed ME !!!\n");
 			}else{
-			writeLine("I'm not hungry right now");
+			writeLine("I'm not hungry right now\n");
 			}
 			break;
 			
 		case "2":
-			dragon.water();
+			//Give Drago water
+			dragon.water();	
 			//Drago is thirsty
 			if (dragon.isThirsty()){
-				writeLine("I need WATER !!!");
+				writeLine("I need WATER !!!\n");
 			}else{
-				writeLine("I'm not thirsty right now");
+				writeLine("I'm not thirsty right now\n");
 			}
 			break;
 		case "3":
-			// Drago is bored 
+			// Play with Drago 
 			dragon.play();
-			
+			// Drago is bored 
 			if (dragon.isBored()){
-				writeLine("I WANNA to Play !!!");
+				writeLine("I WANNA to Play !!!\n");
 			}else{
-				writeLine("I don't wanna play right now");
+				writeLine("I don't wanna play right now\n");
 			}
 			break;
 		case "4":
-			//let Drago rest
+			//Give Drago a nap
+			dragon.nap();
+			//Drago is tired
+			if (dragon.isTired()){
+				writeLine("Yawn, I Need a nap !!!\n");
+			}else{
+				writeLine("Leave me alone I don't want a nap\n");
+			}
 			break;
 		case "5":
 			writeLine(" You have exited the app");
@@ -87,10 +89,12 @@ public class VirtualPetApp {
 			
 		}
 
-		//writeLine("After time moves forward...tick");
-		//writeLine("Hunger: " + dragon.getHunger());
-		//writeLine("Thirst: " + dragon.getThirst());
-		//writeLine("Boredom:"  + dragon.getPlayfulness());
+		//test line
+		writeLine("Before Tick");
+		writeLine("Hunger: " + dragon.getHunger());
+		writeLine("Thirst: " + dragon.getThirst());
+		writeLine("Boredom:"  + dragon.getPlayfulness());
+		writeLine("Tiredness:" + dragon.getTiredness() +"\n");
 				
 		dragon.tick();
 		
@@ -112,15 +116,12 @@ public class VirtualPetApp {
 		writeLine("Hunger: " + dragon.getHunger());
 		writeLine("Thirst: " + dragon.getThirst());
 		writeLine("Boredom:"  + dragon.getPlayfulness());
+		writeLine("Tiredness:" + dragon.getTiredness());
 				
-	 //  writeLine(" so sorry, your pet died");	
 			
 		}//end while
 				
-
-		//test current hunger
-		//writeInt(dragon.getHunger());
-	
+		input.close();
 		
 	}//end main method bracket
 	
@@ -131,6 +132,7 @@ public class VirtualPetApp {
 	public static void writeInt(int message){
 		System.out.println(message);
 	}
+	
 	
 	
 }//end class bracket 
